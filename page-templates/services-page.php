57faf5072b2229d05d09df16b2d488cc8987bf91
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2016-02-02 11:42:11 +0000

    Add fav icon to theme and various others.

--- page-templates/services-page.php
+++ page-templates/services-page.php
@@ -79,6 +79,8 @@ Template Name: Personal Services page template
 								<?php the_content(); ?>
 							</div>
 							
+							<?php include (STYLESHEETPATH . '/_/inc/service-page/faqs.php'); ?>
+							
 							<?php include (STYLESHEETPATH . '/_/inc/service-page/footer-info.php'); ?>
 																		
 						</div>
